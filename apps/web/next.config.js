bad0215b67552ddab092bffab3dbc64612cb04b8
Kaue Machado <kaumac@gmail.com>
2024-03-31 03:04:46 -0300

    wip

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,52 +1,48 @@
 /** @type {import('next').NextConfig} */
 
-const { withTamagui } = require("@tamagui/next-plugin");
-const { join } = require("path");
-const million = require("million/compiler");
-const pattycake = require("pattycake");
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
+const { withTamagui } = require('@tamagui/next-plugin')
+const { join } = require('path')
+const million = require('million/compiler')
+const pattycake = require('pattycake')
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
-  sw: "service-worker.js",
+  sw: 'service-worker.js',
   swcMinify: true,
-});
+})
 
 const boolVals = {
   true: true,
   false: false,
-};
+}
 
 const disableExtraction =
-  boolVals[process.env.DISABLE_EXTRACTION] ??
-  process.env.NODE_ENV === "development";
+  boolVals[process.env.DISABLE_EXTRACTION] ?? process.env.NODE_ENV === 'development'
 
 const enableMillionJS =
-  boolVals[process.env.ENABLE_MILLION_JS] ??
-  process.env.NODE_ENV === "production";
+  boolVals[process.env.ENABLE_MILLION_JS] ?? process.env.NODE_ENV === 'production'
 
 // Temporarily disabled, produces chatty logs
 const enablePattyCake =
-  boolVals[process.env.ENABLE_PATTY_CAKE] ??
-  process.env.NODE_ENV === "production";
+  boolVals[process.env.ENABLE_PATTY_CAKE] ?? process.env.NODE_ENV === 'production'
 
 const plugins = [
   withPWA,
   withTamagui({
-    config: "./tamagui.config.ts",
-    components: ["tamagui", "@imoblr/ui"],
-    importsWhitelist: ["constants.js", "colors.js"],
-    outputCSS:
-      process.env.NODE_ENV === "production" ? "./public/tamagui.css" : null,
+    config: './tamagui.config.ts',
+    components: ['tamagui', '@imoblr/ui'],
+    importsWhitelist: ['constants.js', 'colors.js'],
+    outputCSS: process.env.NODE_ENV === 'production' ? './public/tamagui.css' : null,
     logTimings: true,
     disableExtraction,
     shouldExtract: (path) => {
-      if (path.includes(join("packages", "app"))) {
-        return true;
+      if (path.includes(join('packages', 'app'))) {
+        return true
       }
     },
   }),
-];
+]
 
 module.exports = function () {
   /** @type {import('next').NextConfig} */
@@ -62,42 +58,42 @@ module.exports = function () {
       ignoreBuildErrors: true,
     },
     modularizeImports: {
-      "@tamagui/lucide-icons": {
-        transform: "@tamagui/lucide-icons/dist/esm/icons/{{kebabCase member}}",
+      '@tamagui/lucide-icons': {
+        transform: '@tamagui/lucide-icons/dist/esm/icons/{{kebabCase member}}',
         skipDefaultConversion: true,
       },
     },
     transpilePackages: [
-      "solito",
-      "react-native-web",
-      "expo-linking",
-      "expo-constants",
-      "expo-modules-core",
-      "react-native-safe-area-context",
-      "react-native-reanimated",
-      "react-native-gesture-handler",
+      'solito',
+      'react-native-web',
+      'expo-linking',
+      'expo-constants',
+      'expo-modules-core',
+      'react-native-safe-area-context',
+      'react-native-reanimated',
+      'react-native-gesture-handler',
     ],
-  };
+  }
 
   for (const plugin of plugins) {
     config = {
       ...config,
       ...plugin(config),
-    };
+    }
   }
 
   const millionConfig = {
     auto: true,
     mute: true,
-  };
+  }
 
   if (enableMillionJS) {
-    config = million.next(config, millionConfig);
+    config = million.next(config, millionConfig)
   }
 
   if (enablePattyCake) {
-    config = pattycake.next(config);
+    config = pattycake.next(config)
   }
 
-  return config;
-};
+  return config
+}
