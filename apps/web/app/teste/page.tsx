153198f5ec48ec952d93a3366864dc306108527e
Kaue Machado <kaumac@gmail.com>
2024-04-04 14:41:19 -0300

    tweaks

---
+++ apps/web/app/teste/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { PageRowLayout } from '@/components'
+import { CircleChevronRightFilledIcon } from '@/icons'
+import { Box, Button, Center, H2, Text } from '@/ui'
+
+export default function ImoveisPage() {
+  return <div>sokdfsdo</div>
+}
