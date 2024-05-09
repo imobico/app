dee1c2532bde6d38fc1ffc4f164a38159d8f41f7
Kaue Machado <kaumac@gmail.com>
2024-05-09 16:05:16 -0300

    tweaks

---
+++ apps/mobile/babel.config.js
@@ -0,0 +1,32 @@
+module.exports = (api) => {
+  api.cache(true)
+  return {
+    presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
+    plugins: [
+      require.resolve('expo-router/babel'),
+      [
+        require.resolve('babel-plugin-module-resolver'),
+        {
+          root: ['../..'],
+          alias: {
+            '@imoblr/config': '../../packages/config',
+            '@imoblr/shared': '../../packages/shared',
+            '@imoblr/ui': '../../packages/ui',
+          },
+          extensions: ['.js', '.jsx', '.tsx', '.ios.js', '.android.js'],
+        },
+      ],
+      // if you want reanimated support
+      // 'react-native-reanimated/plugin',
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['@imoblr/ui', '@imoblr/shared', 'tamagui'],
+          config: '../../packages/config/src/tamagui.config.ts',
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === 'development',
+        },
+      ],
+    ],
+  }
+}
